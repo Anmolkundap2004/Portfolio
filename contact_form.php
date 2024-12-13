337ce63a3097ec65bff17a0b42c3af26ab7e48bc
Anmolkundap2004 <4bd22cs017@bietdvg.edu>
2024-12-13 22:19:07 +0530

    Initial commit

--- contact_form.php
+++ contact_form.php
@@ -1,38 +1,54 @@
 <?php
-$servername = "localhost"; // Hostname for the MySQL server
-$username = "root";        // MySQL username
-$password = "";            // MySQL password (if any)
-$dbname = "contact_form";  // Database name
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "contact_form";
 
-// Create a connection to the MySQL database
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check if the connection was successful
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 if (isset($_POST['submit'])) {
-    // Retrieve form data
     $first_name = $_POST['first_name'];
     $middle_name = $_POST['middle_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
     $phone_number = $_POST['phone_number'];
 
-    // Prepare and bind the SQL query
     $stmt = $conn->prepare("INSERT INTO contacts (first_name, middle_name, last_name, email, phone_number) VALUES (?, ?, ?, ?, ?)");
     $stmt->bind_param("sssss", $first_name, $middle_name, $last_name, $email, $phone_number);
 
-    // Execute the query
     if ($stmt->execute()) {
         echo "<script>alert('Your message has been sent successfully!');</script>";
     } else {
         echo "<script>alert('There was an error submitting your form. Please try again.');</script>";
     }
 
-    // Close the statement and connection
     $stmt->close();
-    $conn->close();
 }
+
+if (isset($_POST['show'])) {
+    $query = "SELECT first_name, last_name, email, phone_number FROM contacts";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        echo '<table class="data-table">';
+        echo '<tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Phone Number</th></tr>';
+        while ($row = $result->fetch_assoc()) {
+            echo '<tr>';
+            echo '<td>' . $row['first_name'] . '</td>';
+            echo '<td>' . $row['last_name'] . '</td>';
+            echo '<td>' . $row['email'] . '</td>';
+            echo '<td>' . $row['phone_number'] . '</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+    } else {
+        echo "<p>No records found.</p>";
+    }
+}
+
+$conn->close();
 ?>
